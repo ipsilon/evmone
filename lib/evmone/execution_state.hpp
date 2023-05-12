673de561638d898de0811743560db0573c531078
Paweł Bylica <pawel@ethereum.org>
2023-05-12 10:07:32 +0200

    Revert "Move stack bottom to state"
    This reverts commit 818c24649873a20de14ad41d904b7873f723bbba.
    
    # Conflicts:
    #	lib/evmone/caterpillar.cpp
    #	lib/evmone/execution_state.hpp

--- lib/evmone/execution_state.hpp
+++ lib/evmone/execution_state.hpp
@@ -137,7 +137,6 @@ struct TransactionInitcode
 class ExecutionState
 {
 public:
-    const uint256* stack_bottom = nullptr;
     int64_t gas_left = 0;
     int64_t gas_refund = 0;
     Memory memory;
@@ -183,7 +182,6 @@ public:
         const evmc_host_interface& host_interface, evmc_host_context* host_ctx,
         bytes_view _code) noexcept
     {
-        stack_bottom = nullptr;
         gas_refund = 0;
         memory.clear();
         msg = &message;
