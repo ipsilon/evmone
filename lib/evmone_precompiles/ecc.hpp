b1c7a3f18ac170950866bf876b89a2807c92ac96
Paweł Bylica <pawel@ethereum.org>
2025-09-11 16:08:14 +0200

    crypto: Detect EC point doubling more efficiently

--- lib/evmone_precompiles/ecc.hpp
+++ lib/evmone_precompiles/ecc.hpp
@@ -269,9 +269,6 @@ ProjPoint<Curve> add(const ProjPoint<Curve>& p, const ProjPoint<Curve>& q) noexc
     if (q == 0)
         return p;  // TODO: Untested.
 
-    if (p == q)
-        return dbl(p);
-
     // Use the "add-1998-cmo-2" formula for curve in Jacobian coordinates.
     // The cost is 12M + 4S + 6add + 1*2.
     // https://www.hyperelliptic.org/EFD/g1p/auto-shortw-jacobian.html#addition-add-1998-cmo-2
@@ -292,7 +289,13 @@ ProjPoint<Curve> add(const ProjPoint<Curve>& p, const ProjPoint<Curve>& q) noexc
     const auto s2 = y2 * z1z1z1;
     const auto h = u2 - u1;
     const auto r = s2 - s1;
-    assert(h != 0 || r != 0);  // We already handled p == q case above.
+
+    // Handle point doubling in case p == q, i.e. when u1 == u2 and s1 == s2.
+    // TODO: (r == 0) => (h == 0) so we don't have to check h.
+    assert(r != 0 || h == 0);
+    if (h == 0 && r == 0) [[unlikely]]
+        return dbl(p);
+
     const auto hh = h * h;
     const auto hhh = h * hh;
     const auto v = u1 * hh;
