f2ed7249bb2c800159c8d81bb2abc2ed87700e3c
Paweł Bylica <pawel@hepcolgum.band>
2025-12-10 15:58:44 +0100

    Config → Curve

--- lib/evmone_precompiles/bn254.cpp
+++ lib/evmone_precompiles/bn254.cpp
@@ -6,35 +6,6 @@
 
 namespace evmmax::bn254
 {
-namespace
-{
-
-struct Config
-{
-    // Linearly independent short vectors (𝑣₁=(𝑥₁, 𝑦₁), 𝑣₂=(x₂, 𝑦₂)) such that f(𝑣₁) = f(𝑣₂) = 0,
-    // where f : ℤ×ℤ → ℤₙ is defined as (𝑖,𝑗) → (𝑖+𝑗λ), where λ² + λ ≡ -1 mod n. n is bn245 curve
-    // order. Here λ = 0xb3c4d79d41a917585bfc41088d8daaa78b17ea66b99c90dd. DET is (𝑣₁, 𝑣₂) matrix
-    // determinant. For more details see https://www.iacr.org/archive/crypto2001/21390189.pdf
-    static constexpr auto X1 = 0x6f4d8248eeb859fd95b806bca6f338ee_u256;
-    // Y1 should be negative, hence we calculate the determinant below adding operands instead of
-    // subtracting.
-    static constexpr auto MINUS_Y1 = 0x6f4d8248eeb859fbf83e9682e87cfd45_u256;
-    static constexpr auto X2 = 0x6f4d8248eeb859fc8211bbeb7d4f1128_u256;
-    static constexpr auto Y2 = 0x6f4d8248eeb859fd0be4e1541221250b_u256;
-    static constexpr auto LAMBDA = 0xb3c4d79d41a917585bfc41088d8daaa78b17ea66b99c90dd_u256;
-
-    // Sanity checks. More details in the paper.
-    static_assert((uint512{LAMBDA} * LAMBDA + LAMBDA + 1) % Curve::ORDER == 0);
-    static_assert((X1 + (Curve::ORDER - MINUS_Y1) * uint512{LAMBDA}) % Curve::ORDER == 0);
-    static_assert((X2 + Y2 * uint512{LAMBDA}) % Curve::ORDER == 0);
-};
-
-// For bn254 curve and β ∈ 𝔽ₚ endomorphism ϕ : E₂ → E₂ defined as (𝑥,𝑦) → (β𝑥,𝑦) calculates [λ](𝑥,𝑦)
-// with only one multiplication in 𝔽ₚ. BETA value in Montgomery form;
-inline constexpr auto BETA = ecc::FieldElement<Curve>::wrap(
-    20006444479023397533370224967097343182639219473961804911780625968796493078869_u256);
-}  // namespace
-
 static_assert(AffinePoint{} == 0, "default constructed is the point at infinity");
 
 bool validate(const AffinePoint& pt) noexcept
@@ -53,7 +24,7 @@ AffinePoint mul(const AffinePoint& pt, const uint256& c) noexcept
     if (c == 0)
         return {};
 
-    const auto [k1, k2] = ecc::decompose<Config>(c);
+    const auto [k1, k2] = ecc::decompose<Curve>(c);
 
     // Verify k ≡ k1 + λ·k2 (mod r)
 #ifndef NDEBUG
@@ -68,12 +39,12 @@ AffinePoint mul(const AffinePoint& pt, const uint256& c) noexcept
 
         const auto r_k = r.to_mont(c);
 
-        const auto right = r.add(r_k1, r.mul(r_k2, r.to_mont(Config::LAMBDA)));
+        const auto right = r.add(r_k1, r.mul(r_k2, r.to_mont(Curve::LAMBDA)));
         assert(r_k == right);
     }
 #endif
 
-    const auto q = AffinePoint{BETA * pt.x, !k2.first ? pt.y : -pt.y};
+    const auto q = AffinePoint{Curve::BETA * pt.x, !k2.first ? pt.y : -pt.y};
     const auto p = !k1.first ? pt : AffinePoint{pt.x, -pt.y};
 
     const auto pr = msm(k1.second, p, k2.second, q);
