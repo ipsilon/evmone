688215e852509fbfad5019bb19df26e5fbc41e77
Paweł Bylica <pawel@hepcolgum.band>
2026-02-16 13:19:10 +0100

    docstring

--- test/state/precompiles_libsecp256k1.hpp
+++ test/state/precompiles_libsecp256k1.hpp
@@ -8,9 +8,11 @@
 
 namespace evmone::state
 {
+/// Internal libsecp256k1-based implementation of the ECDSA public key recovery.
 bool ecrecover_libsecp256k1(std::span<uint8_t, 64> pubkey, std::span<const uint8_t, 32> hash,
     std::span<const uint8_t, 64> sig_bytes, bool parity) noexcept;
 
+/// Generic precompile API for libsecp256k1-based ecrecover implementation.
 ExecutionResult ecrecover_execute_libsecp256k1(
     const uint8_t* input, size_t input_size, uint8_t* output, size_t output_size) noexcept;
 }  // namespace evmone::state
